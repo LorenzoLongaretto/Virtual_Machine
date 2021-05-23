c725dec3627c01402d3e7eb95d401e483526797e
LorenzoLongaretto <lorenzolongarettolok@gmail.com>
2021-05-23 16:56:05 -0300

    validline

--- Maquina_Virtual/main.c
+++ Maquina_Virtual/main.c
@@ -37,7 +37,7 @@ L=L->sig;
 }*/
 void muestra_por_pantalla(int32_t memoria[],int lineaActual,char auxline[],char comentario[],char auxcte[],char mnem[],char firstword[]){
 int i=lineaActual-5;
-if( strcmp(auxcte,"EQU")!=0 && strcmp(mnem,"\\\\ASM")!=0 &&auxline[0]!=';' && auxline[0]!='\n' ){
+if( strcmp(auxcte,"EQU")!=0 && strcmp(mnem,"\\\\ASM")!=0 && valid_line(auxline) ){
     if (firstword[0]=='\0')
         if(comentario[0]=='\0')
             printf("[%04d]: %02X %02X %02X %02X\t%d:\t%s\n",i,(memoria[lineaActual]& 0xFF000000)>>24,(memoria[lineaActual]& 0x00FF0000)>>16,(memoria[lineaActual]& 0x0000FF00)>>8,memoria[lineaActual]& 0x000000FF,lineaActual+1,auxline);
